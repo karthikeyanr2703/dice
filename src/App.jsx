bf66eb6e41f93feebb09de0bfce297732b2ff3d1
KARTHIKEYAN R <karthikeyanr2703@gmail.com>
2024-03-13 17:57:12 +0530

    starting basic

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import styled from "styled-components";
+
+const Button = styled.button`
+width: 100px;
+cursor: pointer;
+  
+`
+
+function App() {
+
+  return (
+    <>
+   
+  <Button>
+    start now
+  </Button>
+    </>
+  )
+}
+
+export default App
